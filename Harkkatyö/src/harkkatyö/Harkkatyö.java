e0379d5298fc9c79b17f908b3e955d79985fdedd
Joelipoju <joel.jarvinen@edu.turkuamk.fi>
2023-04-20 14:26:01 +0300

    Added JavaDoc and exception-handling
    Added JavaDoc-commentary for all the methods and surrounded
    aloitusmetodi with try-catch for NumberFormatException, since the
    functionality of the program was compromised if user entered a string.

--- Harkkatyö/src/harkkatyö/Harkkatyö.java
+++ Harkkatyö/src/harkkatyö/Harkkatyö.java
@@ -11,8 +11,35 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Harkkatyö-luokka sisältää visailuohjelman, joka käynnistää tietovisailun.
+ * Luokka käyttää Scanner- Random- BufferedReader- FileWriter-luokkia. Luokka
+ * pitää sisällään myös input-poikkeuksien sekä tiedoston luku- ja kirjoitus-
+ * poikkeuksiin tarvittavat try-catch työkalut. Luokka pitää sisällään
+ * main-metodin, sekä viisi alempaa metodia, joista neljä liittyy visailuun, ja
+ * yksi pyytää käyttäjältä palautetta visan jälkeen. Visassa on kolme osaa,
+ * jotka pelaaja voi suorittaa haluamassaan järjestyksessä tai lopettaa
+ * haluamassaan kohdassa. Visailuosan loputtua ohjelma tulostaa pelaajalle tämän
+ * keräämät kokonaispisteet, sekä kysyy haluaako pelaaja jättää palautetta.
+ * 
+ * @author joeli
+ * @date 20/4/2023
+ */
 public class Harkkatyö {
 
+	/**
+	 * Tämä on ohjelman päämetodi, joka hallinnoi sen kulkua. Metodi kysyy
+	 * käyttäjältä, haluaako tämä aloittaa pelin, jonka jälkeen se kutsuu
+	 * aloitusmetodin. Mikäli peli on suoritettu metodi ilmoittaa käyttäjän keräämät
+	 * kokonaispisteet, ja kysyy vielä halukkuutta palautteen antamiseen.
+	 * 
+	 * @param args            ohjelmalle annettavat komentoriviparametrit, ei
+	 *                        käytössä
+	 * @param esko            Scanner-objekti, jota käytetään syötteen lukemiseen
+	 * @param kokonaispisteet pistelaskuri, johon käyttäjän visassa keräämät pisteet
+	 *                        kerätään
+	 * @throws FileNotFoundException jos ohjelman tarvitsemaa tiedostoa ei löydy.
+	 */
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner esko = new Scanner(System.in);
 		int kokonaispisteet = 0;
@@ -43,6 +70,12 @@ public class Harkkatyö {
 
 	}
 
+	/**
+	 * Tämä metodi mahdollistaa käyttäjän palautteen antamisen ja tallentaa annetun
+	 * palautteen palaute.txt-tiedostoon
+	 * 
+	 * @param esko Scanner-objekti, jota käytetään syötteen lukemiseen
+	 */
 	public static void palauteBoksi(Scanner esko) {
 		try {
 			FileWriter kirjoittaja = new FileWriter("palaute.txt");
@@ -56,7 +89,20 @@ public class Harkkatyö {
 		}
 	}
 
-	public static int aloitusmetodi(Scanner esko) throws FileNotFoundException {
+	/**
+	 * Tämä metodi mahdollistaa valitsemisen visailun kolmen eri aihealueen välillä.
+	 * Metodi pitää kirjaa siitä, mitkä aihealueet on tehty boolean arrayn avulla.
+	 * Arrayn elementit ovat samassa järjestyksessä kuin metodit, joilla aihealueita
+	 * kutsutaan. Mikäli aihealue on jo valittu, metodi pyytää käyttäjää valitsemaan
+	 * uudestaan. Jos kaikki aihealueet on suoritettu tai käyttäjä kirjoittaa
+	 * "valmis", metodi palaa main-metodiin
+	 * 
+	 * @param esko Scanner-objekti, jolla luetaan käyttäjän syötettä
+	 * @return palauttaa käyttäjän keräämät kokonaispisteet
+	 * @throws FileNotFoundException mikäli ohjelman tarvitsemaa tiedostoa ei löydy.
+	 * @throws NumberFormatException mikäli käyttäjä syöttää jotain muuta, kuin numeron.
+	 */
+	public static int aloitusmetodi(Scanner esko) throws FileNotFoundException, NumberFormatException {
 		int pisteet = 0;
 		Boolean[] metodiValinnat = { false, false, false, };
 		boolean kaikkiSuoritettu = false;
@@ -67,34 +113,46 @@ public class Harkkatyö {
 			if (valintaStr.equalsIgnoreCase("valmis")) {
 				return pisteet;
 			}
-			int valinta = Integer.parseInt(valintaStr);
-			if (valinta >= 1 && valinta <= 3) {
-				if (!metodiValinnat[valinta - 1]) {
-					metodiValinnat[valinta - 1] = true;
-					switch (valinta) {
-					case 1:
-						pisteet += maanTieto(esko);
-						break;
-					case 2:
-						pisteet += mateMatiikka(esko);
-						break;
-					case 3:
-						pisteet += suomenKieli(esko);
-						break;
-					}
-					if (Arrays.stream(metodiValinnat).allMatch(valittu -> valittu)) {
-						kaikkiSuoritettu = true;
+			try {
+				int valinta = Integer.parseInt(valintaStr);
+				if (valinta >= 1 && valinta <= 3) {
+					if (!metodiValinnat[valinta - 1]) {
+						metodiValinnat[valinta - 1] = true;
+						switch (valinta) {
+						case 1:
+							pisteet += maanTieto(esko);
+							break;
+						case 2:
+							pisteet += mateMatiikka(esko);
+							break;
+						case 3:
+							pisteet += suomenKieli(esko);
+							break;
+						}
+						if (Arrays.stream(metodiValinnat).allMatch(valittu -> valittu)) {
+							kaikkiSuoritettu = true;
+						}
+					} else {
+						System.out.println("Olet jo suorittanut aihealueen. Valitse jokin toinen aihe.");
 					}
 				} else {
-					System.out.println("Olet jo suorittanut aihealueen. Valitse jokin toinen aihe.");
+					System.out.println("Valitseppa uudestaan.");
 				}
-			} else {
-				System.out.println("Valitseppa uudestaan.");
+			} catch (NumberFormatException e) {
+				System.out.println("Valitseppa uudestaan ja anna numeroarvo väliltä 1-3.");
 			}
 		}
 		return pisteet;
 	}
 
+	/**
+	 * Tämä metodi on kyselee käyttäjältä kieleen liittyviä kysymyksiä, ja jakaa
+	 * pisteitä oikein menneiden vastausten mukaan. Jokainen oikea vastaus on yhden
+	 * pisteen arvoinen, ja lopulta metodi palauttaa kokonaispistemäärän.
+	 * 
+	 * @param esko Scanner-objekti, jolla käyttäjän syötettä luetaan
+	 * @return Käyttäjän osiosta keräämät pisteet
+	 */
 	public static int suomenKieli(Scanner esko) {
 		int pisteet = 0;
 		String rivi = "";
@@ -115,7 +173,8 @@ public class Harkkatyö {
 		} else {
 			System.out.println("Väärin meni :(");
 		}
-		System.out.println("Mihin aikaan loru tapahtuu? Syötä sana perusmuodossa\n(Huom. Oikea vastaus on vain yksi sana!");
+		System.out.println(
+				"Mihin aikaan loru tapahtuu? Syötä sana perusmuodossa\n(Huom. Oikea vastaus on vain yksi sana!");
 		String vastaus2 = esko.nextLine();
 		if (vastaus2.equalsIgnoreCase("aamurusko")) {
 			System.out.println("Oikein meni! Hienoa");
@@ -147,10 +206,20 @@ public class Harkkatyö {
 		} else {
 			System.out.println("Väärin meni :(");
 		}
-		System.out.println("Visan aidinkieliosa on nyt ohi! Sait osiosta " + pisteet + "/5 pistettä. Poistutaan osiosta");
+		System.out
+				.println("Visan aidinkieliosa on nyt ohi! Sait osiosta " + pisteet + "/5 pistettä. Poistutaan osiosta");
 		return pisteet;
 	}
 
+	/**
+	 * Tämä metodi kysyy käyttäjältä viisi laskutoimitusta. Käyttäjän tulee syöttää
+	 * vastaukset numeroina Jokainen kysymys on kääritty try-catchiin, jotta
+	 * MissmatcInput-errorilta vältytään ja ohjelman toiminta ei keskeydy. Lopussa
+	 * metodi palauttaa käyttäjän osiosta keräämät pisteet
+	 * 
+	 * @param esko Scanner-objekti, jota käytetään syötteen lukemiseen
+	 * @return Käyttäjän osiosta keräämät pisteet
+	 */
 	public static int mateMatiikka(Scanner esko) {
 		int pisteet = 0;
 		System.out.println(
@@ -233,6 +302,15 @@ public class Harkkatyö {
 
 	}
 
+	/**
+	 * Tämä metodi lukee tiedostosta "maat.txt" listan maista ja niiden
+	 * pääkaupungeista. Sen jälkeen ohjelma kyselee käyttäjältä maita ja niiden
+	 * pääkaupunkeja.
+	 * 
+	 * @param esko Scanner-objekti, jota käytetään syötteen lukemiseen
+	 * @return pisteet, jotka käyttäjä kerää osiosta.
+	 * @throws FileNotFoundException mikäli ohjelman vaatimaa tiedostoa ei löydetä.
+	 */
 	public static int maanTieto(Scanner esko) throws FileNotFoundException {
 		int määrä = 5;
 		String tiedostonimi = "maat.txt";
